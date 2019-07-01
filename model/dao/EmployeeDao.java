dc9a1ea048a9084e8c95955e3318de9ccc6169d8
reza <reza.khani1998@gmail.com>
2019-07-01 15:45:32 +0430

    create class EmployeeDao

---
+++ model/dao/EmployeeDao.java
@@ -0,0 +1,15 @@
+package ir.maktab.HomeWork11_DataBase.model.dao;
+
+import ir.maktab.HomeWork11_DataBase.Base.BaseDao;
+import ir.maktab.HomeWork11_DataBase.model.Employee;
+
+public interface EmployeeDao extends BaseDao<Employee> {
+
+    Double maxSalaryByCity(String city);
+
+    Employee employeeMaxSalaryByCity(String city);
+
+    Employee findEmpByPostalCode(String postalCode);
+
+    Employee findEmpByTelNumber(String telNumber);
+}
